2b0578e8d2b417a7656a654d486c428eed3a674c
Abián Izquierdo Montesdeoca <abizmo@gmail.com>
2022-04-07 15:59:41 +0100

    Feat/destination page (#1)
    Destination [moon] page all device

---
+++ index.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("#primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// when someone clicks the button menu
+navToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false") {
+    // if the menu is closed, open it
+    nav.setAttribute("data-visible", "true");
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    // if the menu is open, close it
+    nav.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+});
